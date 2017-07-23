d0c22bcd68bf13e311984e846fe22947a010ae93
mahmoud-abdallah-cs <mahmoud.abdallah.cs@gmail.com>
2017-07-23 16:22:21 +0200

    add make method

--- src/ArrayCollection.php
+++ src/ArrayCollection.php
@@ -3,10 +3,42 @@
 namespace Mix\Essential;
 
 use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+use Traversable;
 
-class ArrayCollection implements ArrayAccess
+class ArrayCollection implements ArrayAccess, IteratorAggregate
 {
     protected $items = [];
+
+    public function __construct($items = [])
+    {
+        $this->items = $this->getArrayableItems($items);
+    }
+
+    public static function make($items = [])
+    {
+        return new static($items);
+    }
+
+    protected function getArrayableItems($items)
+    {
+        if (is_array($items)) { return $items; }
+        elseif ($items instanceof self) { return $items->all(); }
+        elseif ($items instanceof Traversable) { return iterator_to_array($items); }
+
+        elseif ($items instanceof Arrayable) { return $items->toArray(); }
+        elseif ($items instanceof Jsonable) { return json_decode($items->toJson(), true); }
+        elseif ($items instanceof JsonSerializable) { return $items->jsonSerialize(); }
+
+        return (array) $items;
+    }
+
+    /* IteratorAggregate Methods */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->items);
+    }
 
     /* ArrayAccess Methods */
     public function offsetExists($offset) { return array_key_exists($offset, $this->items); }
